4b76c95687327c94b2b4deef5080e19ef1802433
claudioseccia <cla.seccia@gmail.com>
2021-04-21 11:56:20 +0200

    ch5

--- src/__tests__/favorite-number.test.js
+++ src/__tests__/favorite-number.test.js
@@ -1,20 +1,21 @@
-import { getQueriesForElement } from '@testing-library/dom';
+// import { getQueriesForElement } from '@testing-library/dom'; // no more necessary after implementing the  render function of react-testing-library
+// import ReactDOM from 'react-dom'; // no more necessary after implementing the  render function of react-testing-library
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
 import { FavoriteNumber } from '../favorite-number';
 
-// expect.extend({ toHaveAttribute, toHaveTextContent });
-// expect.extend({ jestDOM }); //same as above
+/* 
+// this does the same that the render function of react-testing-library does
+function render(ui) {
+	const container = document.createElement('div');
+	ReactDOM.render(ui, container);
+	// const { getByLabelText } = getQueriesForElement(div);
+	const queries = getQueriesForElement(container);
+	return { container, ...queries };
+} */
 
 test('renders a number input with a label "Favorite number"', () => {
-	const div = document.createElement('div');
-	ReactDOM.render(<FavoriteNumber />, div);
-	const { getByLabelText } = getQueriesForElement(div);
-	// const input = queries.getByLabelText(div, 'Favorite Number');
+	const { getByLabelText } = render(<FavoriteNumber />);
 	const input = getByLabelText(/favorite number/i); //test with regEx
-	// expect(div.querySelector('input')).toHaveAttribute('type', 'number');
 	expect(input).toHaveAttribute('type', 'number'); //same as above
-	// expect(div.querySelector('label')).toHaveTextContent('Favorite Number'); //no more necessary
 });
-
-//todo ch5
