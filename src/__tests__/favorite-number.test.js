042f0f8b3858fca5d075407fa32e3b03c06e10eb
claudioseccia <cla.seccia@gmail.com>
2021-04-24 10:37:28 +0200

    ch9 rerender

--- src/__tests__/favorite-number.test.js
+++ src/__tests__/favorite-number.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import user from '@testing-library/user-event';
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { FavoriteNumber } from '../favorite-number';
 
 test('renders a number input with a label "Favorite number"', () => {
@@ -11,10 +11,11 @@ test('renders a number input with a label "Favorite number"', () => {
 });
 // ch7
 test('enter an invalid number shows an error message', () => {
-	const { getByLabelText, getByRole } = render(<FavoriteNumber />);
+	const { getByLabelText, getByRole, rerender, debug } = render(<FavoriteNumber />);
 	const input = getByLabelText(/favorite number/i);
-	// fireEvent.change(input, { target: { value: 10 } });
-	//user instead of fireEvent (same result):
 	user.type(input, '10');
 	expect(getByRole('alert')).toHaveTextContent(/the number is invalid/i); //get this: <div role="alert">The number is invalid</div>
+	debug();
+	rerender(<FavoriteNumber max={10} />); //rerender the same component with different props to test other values
+	debug();
 });
