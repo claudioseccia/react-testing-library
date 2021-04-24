eb52dbc8b4a0e19b73b1d9afc5deda57d893c7c3
claudioseccia <cla.seccia@gmail.com>
2021-04-24 11:49:35 +0200

    ch11 jest-axe/extend-expect import

--- src/__tests__/a11y.test.js
+++ src/__tests__/a11y.test.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 //axe: specific testing module for testing accessibility of some container
-import { axe, toHaveNoViolations } from 'jest-axe';
-expect.extend(toHaveNoViolations);
+import { axe } from 'jest-axe';
+import 'jest-axe/extend-expect';
+// expect.extend(toHaveNoViolations);
 
 const Form = () => {
 	//here form is NOT accessible because the input is not labeled
 	return (
 		<form>
-			<input placeholder="email" />
+			<label htmlFor="email">Email</label>
+			<input id="email" placeholder="email" />
 		</form>
 	);
 };
@@ -16,7 +18,7 @@ const Form = () => {
 test('the form is accessible', async () => {
 	const { container } = render(<Form />);
 	//console.log(container.innerHTML);
-	const results = await axe(container);
+	const results = await axe(container); //async function that returns a promise
 	//console.log(results);
 	//expect(results.violations).toHaveLength(0)
 	expect(results).toHaveNoViolations();
