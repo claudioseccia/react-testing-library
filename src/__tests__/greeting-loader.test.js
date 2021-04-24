ce7399cd83638891042abf353c74095eb8f22f81
claudioseccia <cla.seccia@gmail.com>
2021-04-24 20:45:22 +0200

    ch13 solution for env not supporting jest.mock (ex.storybook)

--- src/__tests__/greeting-loader.test.js
+++ src/__tests__/greeting-loader.test.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import { render, fireEvent, waitFor } from '@testing-library/react';
 import { loadGreeting as mockLoadGreeting } from '../api';
-import { GreetingLoader } from '../greeting-loader-01-mocking';
+import { GreetingLoader } from '../greeting-loader-02-dependency-injection';
 
-//to test we mock the api file called from greeting-loader-01-mocking.js
-jest.mock('../api'); //mock the server request with expected response not to make actual server requests
-//we'll take all the functions from api and mock them
+//solution for any environment that do support jest.mock (ex.storybook)
 
 test('loads greetings on click', async () => {
+	const mockLoadGreeting = jest.fn();
 	const testGreeting = 'TEST_GREETING';
 	mockLoadGreeting.mockResolvedValueOnce({ data: { greeting: testGreeting } }); //mocked version of loadGreeting from '../api.js'
-	const { getByLabelText, getByText } = render(<GreetingLoader />);
+	const { getByLabelText, getByText } = render(<GreetingLoader loadGreeting={mockLoadGreeting} />);
 	const nameInput = getByLabelText(/name/i); //get element by its id: <input id="name" />
 	const loadButton = getByText(/load/i); //get button by its text content
 	nameInput.value = 'Mary'; //fill the name input with some value
@@ -19,4 +18,3 @@ test('loads greetings on click', async () => {
 	expect(mockLoadGreeting).toHaveBeenCalledTimes(1); //make sure it is called once!!!
 	await waitFor(() => expect(getByLabelText(/greeting/i)).toHaveTextContent(testGreeting));
 });
-//todo ch 13
